6efa116fb707f3c00a369379a29b6bc257f6e78c
Gennadiy <renardes@gmail.com>
2025-04-05 22:33:22 +0500

    2025.03.09: оформление

--- 2025.03.09/5.py
+++ 2025.03.09/5.py
@@ -2,8 +2,10 @@ i = input()
 f = input()
 miles = float(i + '.' + f)
 
-print(f'{miles} миль = {(miles * 1.61):.1f} км')
+print(f'{miles} миль = {miles * 1.61:.1f} км')
+
 
 # 129
 # 6
 # 129.6 миль = 208.7 км
+
